9237c649602649251f1d18e75dd2db4a669a52c3
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-11-28 01:54:12 +0200

    Script for simulating noisy XOR

--- bnn_mcmc_examples/mlp/noisy_xor/data.py
+++ bnn_mcmc_examples/mlp/noisy_xor/data.py
@@ -4,9 +4,12 @@ from torch.utils.data import DataLoader
 
 from eeyore.datasets import XYDataset
 
+from bnn_mcmc_examples.datasets import data_paths
+
 from .constants import dtype
 
 # %% Load XOR data
 
-xor = XYDataset.from_eeyore('xor', dtype=dtype)
-dataloader = DataLoader(xor, batch_size=len(xor))
+# xor = XYDataset.from_eeyore('xor', dtype=dtype)
+noisy_xor = XYDataset.from_file(path=data_paths['noisy_xor'], dtype=dtype)
+dataloader = DataLoader(noisy_xor, batch_size=len(noisy_xor))
--- bnn_mcmc_examples/mlp/noisy_xor/generate_data.py
+++ bnn_mcmc_examples/mlp/noisy_xor/generate_data.py
@@ -17,9 +17,13 @@ x = np.vstack([
     np.column_stack([np.random.rand(n) + s, np.random.rand(n) + s])  # points corresponding to (1, 1)
 ])
 
+y = np.repeat([0, 1, 1, 0], n)
+
 # %% Save simulated noisy XOR points
 
 np.savetxt(data_paths['noisy_xor'].joinpath('x.csv'), x, delimiter=',', header='x1,x2', comments='')
+
+np.savetxt(data_paths['noisy_xor'].joinpath('y.csv'), y, fmt='%d', header='y', comments='')
 
 # %% Plot noisy XOR points
 
