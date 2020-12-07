c9de4c574320678d8bd1dbcff714c2152a37c2e9
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-12-07 10:23:38 +0200

    Added first installation files

---
+++ installation/dev.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Start up script for setting up environment on Ubuntu 20.04 LTS
+
+export PKGNAME='bnn_mcmc_examples'
+export PYVERSION='3.7'
+export CONDADIR="$HOME/opt/continuum/miniconda/miniconda3"
+export PYPKGDIR="$HOME/opt/python/packages"
+export CONDAENV="$CONDADIR/envs/$PKGNAME"
+export CONDABIN="$CONDADIR/bin/conda"
+export CONDASCRIPT='Miniconda3-latest-Linux-x86_64.sh'
+
+declare -a PKGNAMES=('kanga' 'eeyore' 'bnn_mcmc_examples')
+
+pkgurl () {
+  echo "https://github.com/papamarkou/$1.git"
+}
+
+pkgdevreqs () {
+  echo "$PYPKGDIR/$1/installation/requirements_$1.txt"
+}
+
+sudo apt-get update
+
+sudo apt-get install tree
+
+wget https://repo.anaconda.com/miniconda/$CONDASCRIPT
+chmod u+x $CONDASCRIPT
+
+$SHELL $CONDASCRIPT -b -p $CONDADIR
+
+$CONDABIN create -n $PKGNAME -y python=$PYVERSION
+
+$CONDABIN init $(basename $SHELL)
+$CONDABIN config --set auto_activate_base false
+
+mkdir -p $PYPKGDIR
+
+for pkg in "${PKGNAMES[@]}"
+do
+   git -C $PYPKGDIR clone $(pkgurl $pkg)
+   $CONDABIN run -p $CONDAENV pip install -e $PYPKGDIR/$pkg -r $(pkgdevreqs $pkg)
+done
+
+rm $HOME/$CONDASCRIPT
--- setup.py
+++ setup.py
@@ -21,19 +21,17 @@ setup(
     packages=find_packages(),
     license='MIT',
     author='Theodore Papamarkou',
-    author_email='theodoros.papamarkou@manchester.ac.uk',
+    author_email='theodore.papamarkou@gmail.com',
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3'
     ],
     keywords=['Bayesian', 'MCMC', 'Monte Carlo', 'neural networks'],
-
-    install_requires=['numpy', 'pandas', 'torch>=1.6.0', 'eeyore>=0.0.9', 'kanga>=0.0.8', 'matplotlib'],
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.19.2', 'pandas', 'torch>=1.6.0', 'eeyore>=0.0.14', 'kanga>=0.0.17', 'matplotlib>=3.3.3'],
     package_data={'bnn_mcmc_examples': ['data/*/x.csv', 'data/*/y.csv', 'data/*/readme.md']},
     include_package_data=True,
     zip_safe=False
