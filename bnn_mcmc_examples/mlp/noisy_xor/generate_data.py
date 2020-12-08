0f7693327dfc387fae74846aafee15eedfa7d50f
theodoros.papamarkou@manchester.ac.uk <t63177tp@hlogin1.pri.csf3.alces.network>
2020-12-08 11:13:39 +0000

    Fix in data generation

--- bnn_mcmc_examples/mlp/noisy_xor/generate_data.py
+++ bnn_mcmc_examples/mlp/noisy_xor/generate_data.py
@@ -5,6 +5,10 @@ import numpy as np
 from bnn_mcmc_examples.datasets import data_paths, simulate_xor
 from bnn_mcmc_examples.mlp.noisy_xor.constants import num_samples
 
+# %% Create data directory if it does not exist
+
+data_paths['noisy_xor'].mkdir(parents=True, exist_ok=True)
+
 # %% Simulate noisy XOR points
 
 x, y = simulate_xor(n=num_samples)
