38670b8adc4419e52183fe8d45067d8a42b7272c
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-12-12 22:49:12 +0200

    Cleaned up pilot run template

--- bnn_mcmc_examples/mlp/noisy_xor/setting1/metropolis_hastings/pilot_visual_summary.py
+++ bnn_mcmc_examples/mlp/noisy_xor/setting1/metropolis_hastings/pilot_visual_summary.py
@@ -5,34 +5,11 @@ import kanga.plots as ps
 from kanga.chains import ChainArray
 
 from bnn_mcmc_examples.mlp.noisy_xor.setting1.metropolis_hastings.constants import sampler_output_pilot_path
+from bnn_mcmc_examples.mlp.noisy_xor.setting1.model import model
 
 # %% Load chain array
 
 chain_array = ChainArray.from_file(keys=['sample', 'accepted'], path=sampler_output_pilot_path)
-
-# %% Generate kanga ChainArray from eeyore ChainList
-
-chain_array = sampler.get_chain().to_kanga()
-
-# %% Compute acceptance rate
-
-print('Acceptance rate: {}'.format(chain_array.acceptance_rate()))
-
-# %% Compute Monte Carlo mean
-
-print('Monte Carlo mean: {}'.format(chain_array.mean()))
-
-# %% Compute Monte Carlo covariance
-
-mc_cov_mat = chain_array.mc_cov()
-
-# %% Compute Monte Carlo standard error
-
-print('Monte Carlo standard error: {}'.format(chain_array.mc_se(mc_cov_mat=mc_cov_mat)))
-
-# %% Compute multivariate ESS
-
-print('Multivariate ESS: {}'.format(chain_array.multi_ess(mc_cov_mat=mc_cov_mat)))
 
 # %% Plot traces of simulated chain
 
