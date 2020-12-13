0d5d70721ba754308fae139ff84ea76f24359e54
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-12-13 23:37:23 +0200

    Fixes in plotting of datasets

--- bnn_mcmc_examples/datasets/noisy_xor/data1/plot_test_data.py
+++ bnn_mcmc_examples/datasets/noisy_xor/data1/plot_test_data.py
@@ -4,13 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from bnn_mcmc_examples.datasets import load_xydataset_from_file
-from bnn_mcmc_examples.datasets.noisy_xor.data1.constants import (
-    num_classes, num_training_samples, training_data_path, output_path
-)
+from bnn_mcmc_examples.datasets.noisy_xor.data1.constants import num_classes, num_test_samples, test_data_path, output_path
 
 # %% Load data
 
-dataset, _ = load_xydataset_from_file(training_data_path)
+dataset, _ = load_xydataset_from_file(test_data_path)
 
 # %% Create output directory if it does not exist
 
@@ -18,7 +16,7 @@ output_path.mkdir(parents=True, exist_ok=True)
 
 # %% Plot noisy XOR points
 
-num_training_samples_cumsum = np.hstack((0, num_training_samples)).cumsum()
+num_test_samples_cumsum = np.hstack((0, num_test_samples)).cumsum()
 
 # print(plt.rcParams['axes.prop_cycle'].by_key()['color'])
 cols = ['#1f77b4', '#ff7f0e', '#d62728', '#e377c2']
@@ -39,8 +37,8 @@ ax.set_box_aspect(1)
 
 for i in range(num_classes):
     ax.plot(
-        dataset.x[num_training_samples_cumsum[i]:num_training_samples_cumsum[i+1], 0],
-        dataset.x[num_training_samples_cumsum[i]:num_training_samples_cumsum[i+1], 1],
+        dataset.x[num_test_samples_cumsum[i]:num_test_samples_cumsum[i+1], 0],
+        dataset.x[num_test_samples_cumsum[i]:num_test_samples_cumsum[i+1], 1],
         'o',
         color=cols[i],
         marker='o',
@@ -51,7 +49,7 @@ for i in range(num_classes):
 plt.legend(labels=labels, loc='right', ncol=1, bbox_to_anchor=(1.4, 0.5), fancybox=True, shadow=True)
 
 plt.savefig(
-    output_path.joinpath('noisy_xor_training_set.jpg'),
+    output_path.joinpath('noisy_xor_test_set.jpg'),
     pil_kwargs={'quality': 100},
     transparent=True,
     bbox_inches='tight',
