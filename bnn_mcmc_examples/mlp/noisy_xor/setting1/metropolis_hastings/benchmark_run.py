249a7333ab9d3d052a688c7b3df6b546063c0dd7
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-12-13 16:27:34 +0200

    Edited bench of setting1, mlp noisy xor, mh

--- bnn_mcmc_examples/mlp/noisy_xor/setting1/metropolis_hastings/benchmark_run.py
+++ bnn_mcmc_examples/mlp/noisy_xor/setting1/metropolis_hastings/benchmark_run.py
@@ -1,5 +1,6 @@
 # %% Import packages
 
+import numpy as np
 import torch
 
 from eeyore.kernels import NormalKernel
@@ -18,7 +19,7 @@ _, dataloader = load_data(dtype=dtype)
 
 # %% Setup proposal variance and proposal kernel for Metropolis-Hastings sampler
 
-proposal_scale = 0.1
+proposal_scale = np.sqrt(0.02)
 
 kernel = NormalKernel(
     torch.zeros(model.num_params(), dtype=model.dtype),
