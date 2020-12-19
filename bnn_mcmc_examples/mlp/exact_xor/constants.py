176854a9319496045daaeec24d579b178361821d
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-12-19 23:57:01 +0200

    Added main structure for exact_xor

---
+++ bnn_mcmc_examples/mlp/exact_xor/constants.py
@@ -0,0 +1,22 @@
+# %% Import packages
+
+import torch
+
+from pathlib import Path
+
+# %% Define constants
+
+output_path = Path.home().joinpath('output', 'bnn_mcmc_examples', 'mlp', 'noisy_xor', 'setting1')
+
+num_features = 2
+
+mlp_dims = [num_features, 2, 1]
+
+dtype = torch.float32
+
+num_chains = 4
+
+num_epochs = 110000
+num_burnin_epochs = 0
+verbose = True
+verbose_step = 1000
---
+++ bnn_mcmc_examples/mlp/exact_xor/metropolis_hastings/benchmark_run.py
@@ -0,0 +1,46 @@
+# %% Import packages
+
+import numpy as np
+import torch
+
+from eeyore.kernels import NormalKernel
+from eeyore.samplers import MetropolisHastings
+
+from bnn_mcmc_examples.datasets import load_xydataset_from_file
+from bnn_mcmc_examples.datasets.noisy_xor.data1.constants import training_data_path
+from bnn_mcmc_examples.mlp.noisy_xor.setting1.constants import (
+    dtype, num_chains, num_epochs, num_burnin_epochs, verbose, verbose_step
+)
+from bnn_mcmc_examples.mlp.noisy_xor.setting1.metropolis_hastings.constants import sampler_output_path
+from bnn_mcmc_examples.mlp.noisy_xor.setting1.model import model
+
+# %% Load dataloader
+
+_, dataloader = load_xydataset_from_file(training_data_path, dtype=dtype)
+
+# %% Setup proposal variance and proposal kernel for Metropolis-Hastings sampler
+
+proposal_scale = np.sqrt(0.02)
+
+kernel = NormalKernel(
+    torch.zeros(model.num_params(), dtype=model.dtype),
+    torch.full([model.num_params()], proposal_scale, dtype=model.dtype)
+)
+
+# %% Setup Metropolis-Hastings sampler
+
+sampler = MetropolisHastings(model, theta0=model.prior.sample(), dataloader=dataloader, kernel=kernel)
+
+# %% Benchmark Metropolis-Hastings sampler
+
+sampler.benchmark(
+    num_chains=num_chains,
+    num_epochs=num_epochs,
+    num_burnin_epochs=num_burnin_epochs,
+    path=sampler_output_path,
+    check_conditions=lambda chain, runtime : 0.15 <= chain.acceptance_rate() <= 0.50,
+    verbose=verbose,
+    verbose_step=verbose_step,
+    print_acceptance=True,
+    print_runtime=True
+)
