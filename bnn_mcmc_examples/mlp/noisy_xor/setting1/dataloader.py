646337212afe0b2ea03e34311476a0e00a0728f5
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-12-22 11:15:34 +0200

    Reorg in dataloader and sampler, template

---
+++ bnn_mcmc_examples/mlp/noisy_xor/setting1/dataloader.py
@@ -0,0 +1,9 @@
+# %% Import packages
+
+from bnn_mcmc_examples.datasets import load_xydataset_from_file
+from bnn_mcmc_examples.datasets.noisy_xor.data1.constants import training_data_path
+from bnn_mcmc_examples.mlp.noisy_xor.setting1.constants import dtype
+
+# %% Load dataloader
+
+_, dataloader = load_xydataset_from_file(training_data_path, dtype=dtype)
--- bnn_mcmc_examples/mlp/noisy_xor/setting1/metropolis_hastings/pilot_run.py
+++ bnn_mcmc_examples/mlp/noisy_xor/setting1/metropolis_hastings/pilot_run.py
@@ -1,41 +1,13 @@
 # %% Import packages
 
-import numpy as np
-import torch
-
 from datetime import timedelta
 from timeit import default_timer as timer
 
-from eeyore.chains import ChainList
-from eeyore.kernels import NormalKernel
-from eeyore.samplers import MetropolisHastings
-
-from bnn_mcmc_examples.datasets import load_xydataset_from_file
-from bnn_mcmc_examples.datasets.noisy_xor.data1.constants import training_data_path
 from bnn_mcmc_examples.mlp.noisy_xor.setting1.constants import (
-    dtype, num_mcmc_burnin_epochs, num_mcmc_epochs, verbose, mcmc_verbose_step
+    num_mcmc_burnin_epochs, num_mcmc_epochs, verbose, mcmc_verbose_step
 )
 from bnn_mcmc_examples.mlp.noisy_xor.setting1.metropolis_hastings.constants import sampler_output_pilot_path
-from bnn_mcmc_examples.mlp.noisy_xor.setting1.model import model
-
-# %% Load dataloader
-
-_, dataloader = load_xydataset_from_file(training_data_path, dtype=dtype)
-
-# %% Setup proposal variance and proposal kernel for Metropolis-Hastings sampler
-
-proposal_scale = np.sqrt(0.02)
-
-kernel = NormalKernel(
-    torch.zeros(model.num_params(), dtype=model.dtype),
-    torch.full([model.num_params()], proposal_scale, dtype=model.dtype)
-)
-
-# %% Setup Metropolis-Hastings sampler
-
-sampler = MetropolisHastings(
-    model, theta0=model.prior.sample(), dataloader=dataloader, kernel=kernel, chain=ChainList(keys=['sample', 'accepted'])
-)
+from bnn_mcmc_examples.mlp.noisy_xor.setting1.metropolis_hastings.sampler import sampler
 
 # %% Run Metropolis-Hastings sampler
 
