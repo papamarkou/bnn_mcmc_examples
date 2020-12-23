4851c4e78091828f4b30ce9fcafe165c996dd629
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-12-23 11:57:53 +0200

    Updated mlp exact xor

--- bnn_mcmc_examples/mlp/exact_xor/metropolis_hastings/benchmark_run.py
+++ bnn_mcmc_examples/mlp/exact_xor/metropolis_hastings/benchmark_run.py
@@ -1,30 +1,10 @@
 # %% Import packages
 
-import numpy as np
-import torch
-
-from eeyore.kernels import NormalKernel
-from eeyore.samplers import MetropolisHastings
-
 from bnn_mcmc_examples.mlp.exact_xor.constants import (
     num_chains, num_mcmc_epochs, num_mcmc_burnin_epochs, verbose, mcmc_verbose_step
 )
-from bnn_mcmc_examples.mlp.exact_xor.dataloader import dataloader
 from bnn_mcmc_examples.mlp.exact_xor.metropolis_hastings.constants import sampler_output_path
-from bnn_mcmc_examples.mlp.exact_xor.model import model
-
-# %% Setup proposal variance and proposal kernel for Metropolis-Hastings sampler
-
-proposal_scale = np.sqrt(3.5)
-
-kernel = NormalKernel(
-    torch.zeros(model.num_params(), dtype=model.dtype),
-    torch.full([model.num_params()], proposal_scale, dtype=model.dtype)
-)
-
-# %% Setup Metropolis-Hastings sampler
-
-sampler = MetropolisHastings(model, theta0=model.prior.sample(), dataloader=dataloader, kernel=kernel)
+from bnn_mcmc_examples.mlp.exact_xor.metropolis_hastings.sampler import sampler
 
 # %% Benchmark Metropolis-Hastings sampler
 
