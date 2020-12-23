98a63cd6e61ac6ffb2460ad03eb13aa711826ead
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-12-23 11:17:38 +0200

    Updated mlp noisy xor setting3

--- bnn_mcmc_examples/mlp/noisy_xor/setting3/metropolis_hastings/benchmark_run.py
+++ bnn_mcmc_examples/mlp/noisy_xor/setting3/metropolis_hastings/benchmark_run.py
@@ -1,35 +1,10 @@
 # %% Import packages
 
-import numpy as np
-import torch
-
-from eeyore.kernels import NormalKernel
-from eeyore.samplers import MetropolisHastings
-
-from bnn_mcmc_examples.datasets import load_xydataset_from_file
-from bnn_mcmc_examples.datasets.noisy_xor.data2.constants import training_data_path
 from bnn_mcmc_examples.mlp.noisy_xor.setting3.constants import (
-    dtype, mcmc_batch_size, num_chains, num_mcmc_epochs, num_mcmc_burnin_epochs, verbose, mcmc_verbose_step
+    num_chains, num_mcmc_epochs, num_mcmc_burnin_epochs, verbose, mcmc_verbose_step
 )
 from bnn_mcmc_examples.mlp.noisy_xor.setting3.metropolis_hastings.constants import sampler_output_path
-from bnn_mcmc_examples.mlp.noisy_xor.setting3.model import model
-
-# %% Load dataloader
-
-_, dataloader = load_xydataset_from_file(training_data_path, dtype=dtype, batch_size=mcmc_batch_size)
-
-# %% Setup proposal variance and proposal kernel for Metropolis-Hastings sampler
-
-proposal_scale = np.sqrt(0.02)
-
-kernel = NormalKernel(
-    torch.zeros(model.num_params(), dtype=model.dtype),
-    torch.full([model.num_params()], proposal_scale, dtype=model.dtype)
-)
-
-# %% Setup Metropolis-Hastings sampler
-
-sampler = MetropolisHastings(model, theta0=model.prior.sample(), dataloader=dataloader, kernel=kernel)
+from bnn_mcmc_examples.mlp.noisy_xor.setting3.metropolis_hastings.sampler import sampler
 
 # %% Benchmark Metropolis-Hastings sampler
 
