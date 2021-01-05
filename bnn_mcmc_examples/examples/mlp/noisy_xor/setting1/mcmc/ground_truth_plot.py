3a4bd87d51d4da6954aecb990b50ccd1a566f0e2
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-05 12:17:20 +0200

    Updated ground truth plot script

--- bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/ground_truth_plot.py
+++ bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/ground_truth_plot.py
@@ -8,6 +8,11 @@ from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.constants import gro
 
 ground_truth_y = np.empty([len(ground_truth_x1), len(ground_truth_x2)])
 
-for x1 in ground_truth_x1:
-    for x2 in ground_truth_x2:
-        print(x1, x2)
+for i in range(len(ground_truth_x1)):
+    for j in range(len(ground_truth_x2)):
+        if ground_truth_x1[i] < 0.5:
+            ground_truth_y[i, j] = 0 if (ground_truth_x2[j] < 0.5) else 1
+        else:
+            ground_truth_y[i, j] = 1 if (ground_truth_x2[j] < 0.5) else 0
+
+print(ground_truth_y)
