3d9f8a9bac75afc8474f43a40147402e91c498ed
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-07 16:34:50 +0200

    Pred posterior benchmark setting 2 mh

--- bnn_mcmc_examples/examples/mlp/noisy_xor/setting2/mcmc/constants.py
+++ bnn_mcmc_examples/examples/mlp/noisy_xor/setting2/mcmc/constants.py
@@ -1,3 +1,7 @@
+# %% Load packages
+
+import numpy as np
+
 # %% Define constants
 
 num_chains = 10
@@ -8,3 +12,21 @@ diagnostic_iter_thres = 10000
 
 verbose = True
 verbose_step = 1000
+
+pred_interval_x_l = -0.5
+pred_interval_x_u = 1.5
+pred_interval_num = 22
+pred_interval_step = (pred_interval_x_u - pred_interval_x_l) / pred_interval_num
+
+pred_interval_x1 = np.linspace(
+    pred_interval_x_l + 0.5 * pred_interval_step,
+    pred_interval_x_u - 0.5 * pred_interval_step,
+    num=pred_interval_num
+)
+pred_interval_x2 = np.linspace(
+    pred_interval_x_l + 0.5 * pred_interval_step,
+    pred_interval_x_u - 0.5 * pred_interval_step,
+    num=pred_interval_num
+)
+
+pred_iter_thres = 100000
