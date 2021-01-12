301f2526eec89bd5b46d5387b27b95fc8eaf1d82
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-12 13:40:41 +0200

    Working towards a Pima dataset

--- downloads/pima.r
+++ downloads/pima.r
@@ -1,13 +1,53 @@
 # Load mlbench library, which contains the Pima dataframe
+
 library(mlbench)
 
+# Load Pima dataframe
+
+data(PimaIndiansDiabetes2)
+
+# Set the seed for splitting the data 
+
+# print("Setting the seed")
+# set.seed(100)
+
+# Dimensions of Pima dataframe
+
+print("Dimensions of dataframe:")
+print(dim(PimaIndiansDiabetes2))
+dim(na.omit(PimaIndiansDiabetes2))
+
 # Check for missing values
-# table(is.na(PimaIndiansDiabetes)) # dataframe-wise
-# sapply(PimaIndiansDiabetes, function(x) sum(is.na(x))) # column-wise
-# rowSums(is.na(PimaIndiansDiabetes)) # row-wise
+
+print("Checking for missing values:")
+print(table(is.na(PimaIndiansDiabetes2))) # dataframe-wise
+# sapply(PimaIndiansDiabetes2, function(x) sum(is.na(x))) # column-wise
+# rowSums(is.na(PimaIndiansDiabetes2)) # row-wise
+
+# 
+
+model <- glm(
+  diabetes ~ pregnant + glucose + pressure + triceps + insulin + mass + pedigree + age,
+  data=PimaIndiansDiabetes2,
+  family="binomial"
+)
+
+print(summary(model))
+
+filtered_dframe <- PimaIndiansDiabetes2[c("glucose", "mass", "pedigree", "age", "diabetes")]
+
+summary(glm(
+  diabetes ~  glucose + mass + pedigree + age,
+  data=PimaIndiansDiabetes2,
+  family="binomial"
+))
 
 # Frequency of diabetes cases
-# table(PimaIndiansDiabetes$diabetes)
 
-out_frame <- PimaIndiansDiabetes
-out_frame$diabetes <- ifelse(PimaIndiansDiabetes$diabetes == "pos", 1, 0)
+print("Frequency of diabetes cases:")
+print(table(PimaIndiansDiabetes2$diabetes))
+
+#
+
+dframe <- PimaIndiansDiabetes2
+dframe$diabetes <- ifelse(PimaIndiansDiabetes2$diabetes == "pos", 1, 0)
---
+++ downloads/split_data.r
@@ -0,0 +1,5 @@
+# Define function for splitting data related to classification
+
+split_data <- function(dframe, freqs) {
+  return(0)
+}
