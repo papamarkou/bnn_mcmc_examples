ee1e4171ba0f7fbcb3c9b26ba3211ae27e27a6df
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-13 11:07:09 +0200

    Cleaned up existing code in pima datafile creation

--- downloads/pima.r
+++ downloads/pima.r
@@ -2,7 +2,7 @@
 
 library(mlbench)
 
-# Load Pima dataframe
+# Load dataframe
 
 data(PimaIndiansDiabetes2)
 
@@ -11,20 +11,42 @@ data(PimaIndiansDiabetes2)
 # print("Setting the seed")
 # set.seed(100)
 
-# Dimensions of Pima dataframe
+# Dimensions of dataframe
 
-print("Dimensions of dataframe:")
+print(c("Dimensions of dataframe:"))
 print(dim(PimaIndiansDiabetes2))
-dim(na.omit(PimaIndiansDiabetes2))
 
-# Check for missing values
+# Frequency of diabetes cases
 
-print("Checking for missing values:")
-print(table(is.na(PimaIndiansDiabetes2))) # dataframe-wise
-# sapply(PimaIndiansDiabetes2, function(x) sum(is.na(x))) # column-wise
-# rowSums(is.na(PimaIndiansDiabetes2)) # row-wise
+print("Frequency of diabetes cases:")
+print(table(PimaIndiansDiabetes2$diabetes))
 
-# 
+# Dimensions of dataframe without missing values
+
+print("Dimensions of dataframe without missing values:")
+print(dim(na.omit(PimaIndiansDiabetes2)))
+
+# Frequency of diabetes cases without missing values
+
+print("Frequency of diabetes cases without missing values:")
+print(table(na.omit(PimaIndiansDiabetes2)$diabetes))
+
+# Missing values per column
+
+print("Missing values per column:")
+print(sapply(PimaIndiansDiabetes2, function(x) sum(is.na(x))))
+
+# Entries with missing values in the dataframe
+
+# print("Entries with missing values in the dataframe:")
+# print(table(is.na(PimaIndiansDiabetes2)))
+
+# Missing values per row
+
+# print("Missing values per row:")
+# print(rowSums(is.na(PimaIndiansDiabetes2)))
+
+# Fit logistic regression to the data
 
 model <- glm(
   diabetes ~ pregnant + glucose + pressure + triceps + insulin + mass + pedigree + age,
@@ -32,22 +54,38 @@ model <- glm(
   family="binomial"
 )
 
+# Print summary of logistic regression
+
+print("Summary of logistic regression:")
 print(summary(model))
 
-filtered_dframe <- PimaIndiansDiabetes2[c("glucose", "mass", "pedigree", "age", "diabetes")]
+# Fit logistic regression to the data using only significant predictors
 
-summary(glm(
-  diabetes ~  glucose + mass + pedigree + age,
-  data=PimaIndiansDiabetes2,
-  family="binomial"
-))
+reduced_model <- glm(diabetes ~  glucose + mass + pedigree + age, data=PimaIndiansDiabetes2, family="binomial")
 
-# Frequency of diabetes cases
+# Print summary of logistic regression using only significant predictors
 
-print("Frequency of diabetes cases:")
-print(table(PimaIndiansDiabetes2$diabetes))
+print("Summary of logistic regression using only significant predictors:")
+print(summary(reduced_model))
 
-#
+# Keep only significant predictors in the dataframe
 
-dframe <- PimaIndiansDiabetes2
-dframe$diabetes <- ifelse(PimaIndiansDiabetes2$diabetes == "pos", 1, 0)
+reduced_dframe <- PimaIndiansDiabetes2[c("glucose", "mass", "pedigree", "age", "diabetes")]
+
+# Keep only data without missing values in the reduced dataframe
+
+reduced_dframe <- na.omit(reduced_dframe)
+
+# Convert factor output variable to numeric
+
+reduced_dframe$diabetes <- ifelse(reduced_dframe$diabetes == "pos", 1, 0)
+
+# Dimensions of reduced dataframe
+
+print(c("Dimensions of reduced dataframe:"))
+print(dim(reduced_dframe))
+
+# Frequency of diabetes cases in reduced dataframe
+
+print("Frequency of diabetes cases in reduced dataframe:")
+print(table(reduced_dframe$diabetes))
