b0f86cfdb8b63dc57414350a2d7b704d1b65a282
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-14 08:39:28 +0200

    Split pima to train and test

--- bnn_mcmc_examples/examples/mlp/pima/datasets.py
+++ bnn_mcmc_examples/examples/mlp/pima/datasets.py
@@ -1,4 +1,16 @@
 # %% Import packages
 
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 
+from bnn_mcmc_examples.datasets import data_paths
+
+# %% Load Pima data
+
+x = pd.read_csv(data_paths['pima'].joinpath('x.csv'))
+y = pd.read_csv(data_paths['pima'].joinpath('y.csv'))
+
+# %% Split data to training and test set
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=100, stratify=y)
