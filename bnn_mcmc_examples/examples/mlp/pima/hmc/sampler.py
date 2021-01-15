a87cf823adb7855f7686d62934211f7639f68a4e
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-16 00:29:29 +0200

    Fix in pima hmc

--- bnn_mcmc_examples/examples/mlp/pima/hmc/sampler.py
+++ bnn_mcmc_examples/examples/mlp/pima/hmc/sampler.py
@@ -2,9 +2,9 @@
 
 from eeyore.samplers import HMC
 
-from bnn_mcmc_examples.examples.mlp.pima.mcmc.dataloaders import training_dataloader
+from bnn_mcmc_examples.examples.mlp.pima.dataloaders import training_dataloader
 from bnn_mcmc_examples.examples.mlp.pima.model import model
 
 # %% Setup HMC sampler
 
-sampler = HMC(model, theta0=model.prior.sample(), dataloader=training_dataloader, step=0.11, num_steps=10)
+sampler = HMC(model, theta0=model.prior.sample(), dataloader=training_dataloader, step=0.05, num_steps=10)
