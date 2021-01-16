5814ea6c07b39067c987b4995e4850385f93cf09
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-16 02:17:43 +0200

    Tweaking pima

--- bnn_mcmc_examples/examples/mlp/pima/constants.py
+++ bnn_mcmc_examples/examples/mlp/pima/constants.py
@@ -1,6 +1,5 @@
 # %% Import packages
 
-import numpy as np
 import torch
 
 from pathlib import Path
@@ -11,41 +10,17 @@ output_path = Path.home().joinpath('output', 'bnn_mcmc_examples', 'mlp', 'pima')
 
 num_features = 4
 
-mlp_dims = [num_features, 2, 1, 2, 1]
-mlp_bias = 4 * [True]
-mlp_activations = 4 * [torch.sigmoid]
-# mlp_dims = [num_features, 2, 1]
-# mlp_bias = 2 * [True]
-# mlp_activations = 2 * [torch.sigmoid]
+mlp_dims = [num_features, 2, 2, 1]
+mlp_bias = 3 * [True]
+mlp_activations = 3 * [torch.sigmoid]
 
 dtype = torch.float32
 
-num_chains = 2 # 10
+num_chains = 1 # 10
 
-# num_epochs = 11000
-num_epochs = 110000
+num_epochs = 11000
 num_burnin_epochs = 0
-# diagnostic_iter_thres = 1000
-diagnostic_iter_thres = 10000
+diagnostic_iter_thres = 1000
 
 verbose = True
 verbose_step = 1000
-
-pred_interval_x_l = -0.5
-pred_interval_x_u = 1.5
-pred_interval_num = 11 # 22
-pred_interval_step = (pred_interval_x_u - pred_interval_x_l) / pred_interval_num
-
-pred_interval_x1 = np.linspace(
-    pred_interval_x_l + 0.5 * pred_interval_step,
-    pred_interval_x_u - 0.5 * pred_interval_step,
-    num=pred_interval_num
-)
-pred_interval_x2 = np.linspace(
-    pred_interval_x_l + 0.5 * pred_interval_step,
-    pred_interval_x_u - 0.5 * pred_interval_step,
-    num=pred_interval_num
-)
-
-# pred_iter_thres = 10000
-pred_iter_thres = 100000
