819e872519d0c2bda5a7817c4d06e4d5c3679a19
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-17 09:13:56 +0200

    Drafted pred posterior on test

--- bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/metropolis_hastings/benchmark_pred_posterior_vals_on_test.py
+++ bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/metropolis_hastings/benchmark_pred_posterior_vals_on_test.py
@@ -1,14 +1,11 @@
 # %% Load packages
 
 import numpy as np
-import torch
 
 from eeyore.chains import ChainLists
 
 from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.constants import dtype
-from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.constants import (
-    num_chains, pred_interval_x1, pred_interval_x2, pred_iter_thres
-)
+from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.constants import num_chains, pred_iter_thres
 from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.metropolis_hastings.constants import sampler_output_run_paths
 from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.model import model
 from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.datascanners import test_dataloader
@@ -24,18 +21,14 @@ for i in range(num_chains):
 
 # %% Generate ground truch
 
-pred_posterior_yhat = np.empty([num_chains, len(pred_interval_x1), len(pred_interval_x2)])
+pred_posterior = np.empty([num_chains, len(test_dataloader)])
 
 for k in range(num_chains):
-    for i in range(len(pred_interval_x1)):
-        for j in range(len(pred_interval_x2)):
-            pred_posterior_yhat[k, i, j] = model.predictive_posterior(
-                chain_lists.vals['sample'][k],
-                torch.tensor([pred_interval_x1[i], pred_interval_x2[j]], dtype=dtype),
-                torch.tensor([1.], dtype=dtype)
-            )
+    print(k)
+    for i, (x, y) in enumerate(test_dataloader):
+        pred_posterior[k, i] = model.predictive_posterior(chain_lists.vals['sample'][k], x.squeeze(), y.squeeze(-1))
 
 # %% Save predictive posteriors
 
 for i in range(num_chains):
-    np.savetxt(sampler_output_run_paths[i].joinpath('pred_posterior_on_grid.csv'), pred_posterior_yhat[i], delimiter=',')
+    np.savetxt(sampler_output_run_paths[i].joinpath('pred_posterior_on_test.csv'), pred_posterior[i], delimiter=',')
