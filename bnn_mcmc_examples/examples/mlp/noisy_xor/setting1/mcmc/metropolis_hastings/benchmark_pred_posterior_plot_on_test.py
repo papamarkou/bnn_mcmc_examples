783198de3ca73f062126ccfc6478061aea31a349
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-18 12:12:56 +0200

    Nearly finished first draft of pred bar plot

--- bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/metropolis_hastings/benchmark_pred_posterior_plot_on_test.py
+++ bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/metropolis_hastings/benchmark_pred_posterior_plot_on_test.py
@@ -1,5 +1,6 @@
 # %% Load packages
 
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -16,20 +17,15 @@ test_data, test_labels = next(iter(test_dataloader))
 
 pred_colors = {'correct': 'green', 'wrong': 'red'}
 
-num_correct_0s = sum(test_logit_dict[0] < 0.5)
-num_correct_1s = sum(test_logit_dict[1] >= 0.5)
-bar_colors = num_correct_0s * [pred_colors['correct']]
-bar_colors.extend((len(test_logit_dict[0]) - num_correct_0s) * [pred_colors['wrong']])
-bar_colors.extend(num_correct_1s * [pred_colors['correct']])
-bar_colors.extend((len(test_logit_dict[1]) - num_correct_1s) * [pred_colors['wrong']])
+patch_list = []
+for key in pred_colors:
+    patch_list.append(mpatches.Patch(color=pred_colors[key], label=key))
 
-['green']
+legend_patches = [mpatches.Patch(color=pred_colors[key], label=key.capitalize()) for key in pred_colors]
 
-for i in range(num_chains):    
-    # Load test logits
+for i in range(num_chains):
     test_logits = np.loadtxt(sampler_output_run_paths[i].joinpath('pred_posterior_on_test.txt'), delimiter=',', skiprows=0)
 
-    # Construct dictionary of test logits
     test_logit_dict = {
         0: np.sort(test_logits[(1 - test_labels.squeeze(-1)).to(dtype=torch.bool)]),
         1: np.sort(test_logits[test_labels.squeeze(-1).to(dtype=torch.bool)])[::-1]
@@ -42,11 +38,30 @@ for i in range(num_chains):
     bar_colors.extend(num_correct_1s * [pred_colors['correct']])
     bar_colors.extend((len(test_logit_dict[1]) - num_correct_1s) * [pred_colors['wrong']])
 
+    plt.figure(figsize=[8, 4])
+
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['axes.titlesize'] = 14
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+
     plt.bar(
         range(len(test_labels)),
         np.hstack([test_logit_dict[0], test_logit_dict[1]]),
-        color=bar_colors
+        width=0.7,
+        color=bar_colors,
+        align='edge'
     )
+
+    plt.legend(handles=legend_patches, loc='upper right', ncol=1)
+
+    plt.axhline(y=0.5, xmin=0, xmax=len(test_labels), color='black', linestyle='dashed', linewidth=1.5)
+
+    plt.axvline(x=0.5*len(test_labels), ymin=0, ymax=1, color='black', linestyle='dotted', linewidth=1.5)
 
     plt.savefig(
         sampler_output_run_paths[i].joinpath('pred_posterior_on_test.png'),
@@ -56,5 +71,4 @@ for i in range(num_chains):
         pad_inches=0.1
     )
 
-    # Save predictions
     plt.close()
