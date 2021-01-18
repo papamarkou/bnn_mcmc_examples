1ef08928de04a220e817e22c58a2a4efc69c44a9
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-18 09:34:27 +0200

    Completed non-polished draft of pred bar plot

--- bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/metropolis_hastings/benchmark_pred_posterior_plot_on_test.py
+++ bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/metropolis_hastings/benchmark_pred_posterior_plot_on_test.py
@@ -12,7 +12,18 @@ from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.metropolis_hastings.
 
 test_data, test_labels = next(iter(test_dataloader))
 
-# %% Make and save predictions
+# %% Plot predictive posteriors
+
+pred_colors = {'correct': 'green', 'wrong': 'red'}
+
+num_correct_0s = sum(test_logit_dict[0] < 0.5)
+num_correct_1s = sum(test_logit_dict[1] >= 0.5)
+bar_colors = num_correct_0s * [pred_colors['correct']]
+bar_colors.extend((len(test_logit_dict[0]) - num_correct_0s) * [pred_colors['wrong']])
+bar_colors.extend(num_correct_1s * [pred_colors['correct']])
+bar_colors.extend((len(test_logit_dict[1]) - num_correct_1s) * [pred_colors['wrong']])
+
+['green']
 
 for i in range(num_chains):    
     # Load test logits
@@ -20,9 +31,22 @@ for i in range(num_chains):
 
     # Construct dictionary of test logits
     test_logit_dict = {
-        0: test_logits[(1 - test_labels.squeeze(-1)).to(dtype=torch.bool)],
-        1: test_logits[test_labels.squeeze(-1).to(dtype=torch.bool)]
+        0: np.sort(test_logits[(1 - test_labels.squeeze(-1)).to(dtype=torch.bool)]),
+        1: np.sort(test_logits[test_labels.squeeze(-1).to(dtype=torch.bool)])[::-1]
     }
+
+    num_correct_0s = sum(test_logit_dict[0] < 0.5)
+    num_correct_1s = sum(test_logit_dict[1] >= 0.5)
+    bar_colors = num_correct_0s * [pred_colors['correct']]
+    bar_colors.extend((len(test_logit_dict[0]) - num_correct_0s) * [pred_colors['wrong']])
+    bar_colors.extend(num_correct_1s * [pred_colors['correct']])
+    bar_colors.extend((len(test_logit_dict[1]) - num_correct_1s) * [pred_colors['wrong']])
+
+    plt.bar(
+        range(len(test_labels)),
+        np.hstack([test_logit_dict[0], test_logit_dict[1]]),
+        color=bar_colors
+    )
 
     plt.savefig(
         sampler_output_run_paths[i].joinpath('pred_posterior_on_test.png'),
