2e9b29fcae571019481307731eb87816ab904109
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-23 23:13:50 +0200

    Template of fix in mean-based predictions

--- bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/metropolis_hastings/benchmark_preds_via_mean.py
+++ bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/metropolis_hastings/benchmark_preds_via_mean.py
@@ -1,12 +1,13 @@
 # %% Load packages
 
 import numpy as np
+import torch
 
 from eeyore.chains import ChainLists
 
 from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.constants import dtype
 from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.constants import num_chains, pred_iter_thres
-from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.dataloaders import test_dataloader
+from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.datascanners import test_dataloader
 from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.mcmc.metropolis_hastings.constants import sampler_output_run_paths
 from bnn_mcmc_examples.examples.mlp.noisy_xor.setting1.model import model
 
@@ -19,25 +20,18 @@ chain_lists = ChainLists.from_file(sampler_output_run_paths, keys=['sample'], dt
 for i in range(num_chains):
     chain_lists.vals['sample'][i] = chain_lists.vals['sample'][i][pred_iter_thres:]
 
-# %% Load test data and labels
-
-test_data, test_labels = next(iter(test_dataloader))
-
 # %% Compute chain means
 
 means = chain_lists.mean()
 
 # %% Make and save predictions
 
-for i in range(num_chains):
-    # Initialize model parameters
-    model.set_params(means[i, :].clone().detach())
+for k in range(num_chains):
+    test_pred_probs = np.empty([len(test_dataloader)])
 
-    # Compute test logits
-    test_logits = model(test_data)
+    for i, (x, _) in enumerate(test_dataloader):
+        test_pred_probs[i] = model.predictive_posterior([means[k, :]], x.squeeze(), torch.tensor([1.], dtype=dtype))
 
-    # Make test predictions
-    test_preds = test_logits.squeeze() > 0.5
+    test_preds = test_pred_probs > 0.5
 
-    # Save predictions
-    np.savetxt(sampler_output_run_paths[i].joinpath('preds_via_mean.txt'), test_preds, fmt='%d', delimiter=',')
+    np.savetxt(sampler_output_run_paths[k].joinpath('preds_via_mean.txt'), test_preds, fmt='%d', delimiter=',')
