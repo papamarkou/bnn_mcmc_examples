695dd98c7c420d4c5661db5666f2b349f5dc957c
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-25 09:57:01 +0200

    Reset constants in noisy xor setting1 mcmc

--- bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/constants.py
+++ bnn_mcmc_examples/examples/mlp/noisy_xor/setting1/mcmc/constants.py
@@ -4,13 +4,11 @@ import numpy as np
 
 # %% Define constants
 
-# num_chains = 2
 num_chains = 10
 
 num_epochs = 110000
 num_burnin_epochs = 0
-diagnostic_iter_thres = 100000
-# diagnostic_iter_thres = 10000
+diagnostic_iter_thres = 10000
 
 verbose = True
 verbose_step = 1000
@@ -31,5 +29,4 @@ pred_interval_x2 = np.linspace(
     num=pred_interval_num
 )
 
-pred_iter_thres = 109000
-# pred_iter_thres = 100000
+pred_iter_thres = 100000
