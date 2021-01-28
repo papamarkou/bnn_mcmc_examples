54f727bdd0030875def5e74cc58a57c58b4f5b4b
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-28 13:53:33 +0200

    Updated noisy xor setting2 MH pred

--- bnn_mcmc_examples/examples/mlp/noisy_xor/setting2/mcmc/metropolis_hastings/benchmark_pred_accuracy_via_bm.py
+++ bnn_mcmc_examples/examples/mlp/noisy_xor/setting2/mcmc/metropolis_hastings/benchmark_pred_accuracy_via_bm.py
@@ -12,17 +12,15 @@ from bnn_mcmc_examples.examples.mlp.noisy_xor.setting2.mcmc.metropolis_hastings.
 
 # %% Load test data and labels
 
-test_data, test_labels = next(iter(test_dataloader))
+_, test_labels = next(iter(test_dataloader))
 
 # %% Compute predictive accuracies
 
 accuracies = np.empty(num_chains)
 
 for i in range(num_chains):
-    # Load test predictions
-    test_preds = np.loadtxt(sampler_output_run_paths[i].joinpath('preds_via_bm.txt'), delimiter=',', skiprows=0)
+    test_preds = np.loadtxt(sampler_output_run_paths[i].joinpath('preds_via_bm.txt'), skiprows=0)
 
-    # Compute test accuracy
     accuracies[i] = accuracy_score(test_preds, test_labels.squeeze())
 
 # %% Save predictive accuracies
