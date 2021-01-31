ce4a19e7290e874634879a9c9fae500fb03bcab5
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-01-31 10:42:16 +0200

    Added prior for pima

---
+++ bnn_mcmc_examples/examples/mlp/pima/setting1/prior/benchmark_pred_accuracy_via_bm.py
@@ -0,0 +1,26 @@
+# %% Load packages
+
+import numpy as np
+
+from sklearn.metrics import accuracy_score
+
+from bnn_mcmc_examples.examples.mlp.pima.setting1.constants import num_chains
+from bnn_mcmc_examples.examples.mlp.pima.setting1.dataloaders import test_dataloader
+from bnn_mcmc_examples.examples.mlp.pima.setting1.prior.constants import sampler_output_path, sampler_output_run_paths
+
+# %% Load test data and labels
+
+_, test_labels = next(iter(test_dataloader))
+
+# %% Compute predictive accuracies
+
+accuracies = np.empty(num_chains)
+
+for i in range(num_chains):
+    test_preds = np.loadtxt(sampler_output_run_paths[i].joinpath('preds_via_bm.txt'), skiprows=0)
+
+    accuracies[i] = accuracy_score(test_preds, test_labels.squeeze())
+
+# %% Save predictive accuracies
+
+np.savetxt(sampler_output_path.joinpath('accuracies_via_bm.txt'), accuracies)
---
+++ bnn_mcmc_examples/examples/mlp/pima/setting1/prior/benchmark_pred_prior_vals_on_test.py
@@ -0,0 +1,53 @@
+# %% Load packages
+
+import numpy as np
+import torch
+
+from eeyore.chains import ChainLists
+
+from bnn_mcmc_examples.examples.mlp.pima.setting1.constants import dtype, num_chains, pred_iter_thres
+from bnn_mcmc_examples.examples.mlp.pima.setting1.datascanners import test_dataloader
+from bnn_mcmc_examples.examples.mlp.pima.setting1.model import model
+from bnn_mcmc_examples.examples.mlp.pima.setting1.prior.constants import sampler_output_run_paths
+
+# %% Load chain lists
+
+chain_lists = ChainLists.from_file(sampler_output_run_paths, keys=['sample'], dtype=dtype)
+
+# %% Drop burn-in samples
+
+for i in range(num_chains):
+    chain_lists.vals['sample'][i] = chain_lists.vals['sample'][i][pred_iter_thres:]
+
+# %% Compute and save predictive priors
+
+verbose_msg = 'Evaluating predictive prior based on chain {:' \
+    + str(len(str(num_chains))) \
+    + '} out of ' \
+    + str(num_chains) \
+    + ' at test point {:' \
+    + str(len(str(len(test_dataloader)))) \
+    + '} out of ' \
+    + str(len(test_dataloader)) \
+    + '...'
+
+for k in range(num_chains):
+    test_pred_probs = np.empty([len(test_dataloader), 2])
+    nums_dropped_samples = np.empty([len(test_dataloader)], dtype=np.int64)
+
+    for i, (x, _) in enumerate(test_dataloader):
+        print(verbose_msg.format(k+1, i+1))
+
+        integral, num_dropped_samples = model.predictive_posterior(
+            chain_lists.vals['sample'][k], x, torch.tensor([[1.]], dtype=dtype)
+        )
+        test_pred_probs[i, 1] = integral.item()
+        test_pred_probs[i, 0] = 1. - test_pred_probs[i, 1]
+        nums_dropped_samples[i] = num_dropped_samples
+
+    np.savetxt(sampler_output_run_paths[k].joinpath('pred_prior_on_test.csv'), test_pred_probs, delimiter=',')
+    np.savetxt(
+        sampler_output_run_paths[k].joinpath('pred_prior_on_test_num_dropped_samples.txt'),
+        nums_dropped_samples,
+        fmt='%d'
+    )
