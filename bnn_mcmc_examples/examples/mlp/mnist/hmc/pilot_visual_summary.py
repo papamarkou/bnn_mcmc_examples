7bb4ecfb33db4c30a8e61e31f528bda0efb24e3d
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2021-08-22 14:06:17 +0100

    Edit in plot script for mnist pilot run

--- bnn_mcmc_examples/examples/mlp/mnist/hmc/pilot_visual_summary.py
+++ bnn_mcmc_examples/examples/mlp/mnist/hmc/pilot_visual_summary.py
@@ -6,7 +6,6 @@ from kanga.chains import ChainArray
 
 from bnn_mcmc_examples.examples.mlp.mnist.constants import diagnostic_iter_thres
 from bnn_mcmc_examples.examples.mlp.mnist.hmc.constants import sampler_output_pilot_path
-from bnn_mcmc_examples.examples.mlp.mnist.model import model
 
 # %% Load chain array
 
@@ -17,9 +16,14 @@ chain_array = ChainArray.from_file(keys=['sample', 'accepted'], path=sampler_out
 chain_array.vals['sample'] = chain_array.vals['sample'][diagnostic_iter_thres:, :]
 chain_array.vals['accepted'] = chain_array.vals['accepted'][diagnostic_iter_thres:]
 
+# %% Set subset of parameters for visual summaries
+
+par_subset = [1, 3000, 8000]
+par_subset[:] = [i-1 for i in par_subset]
+
 # %% Plot traces of simulated chain
 
-for i in range(3): #model.num_params()):
+for i in par_subset:
     ps.trace(
         chain_array.get_param(i),
         title=r'Traceplot of $\theta_{{{}}}$'.format(i+1),
@@ -29,7 +33,7 @@ for i in range(3): #model.num_params()):
 
 # %% Plot running means of simulated chain
 
-for i in range(model.num_params()):
+for i in par_subset:
     ps.running_mean(
         chain_array.get_param(i),
         title=r'Running mean plot of parameter $\theta_{{{}}}$'.format(i+1),
@@ -39,7 +43,7 @@ for i in range(model.num_params()):
 
 # %% Plot histograms of marginals of simulated chain
 
-for i in range(model.num_params()):
+for i in par_subset:
     ps.hist(
         chain_array.get_param(i),
         bins=30,
